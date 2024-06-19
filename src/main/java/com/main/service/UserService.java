2f82967abcf2bad2af7fb07ac741fcc4639edcb0
Ravi Kumar <83981334+cyboravidell@users.noreply.github.com>
2024-06-19 16:28:03 +0530

    Create UserService.java
    User service added to perform business logic.

---
+++ src/main/java/com/main/service/UserService.java
@@ -0,0 +1,14 @@
+package com.main.service;
+
+import java.util.*;
+
+@Service
+public class UserService{
+  private UserRepository userRepository;
+
+public User createNewUser(User user){
+  return new User();
+
+}
+}
+  
